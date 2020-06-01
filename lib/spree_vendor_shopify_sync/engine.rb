3a0fff852e15bdb831be8eb82fcd2c7d854383c3
Ashley <ashleyvdp@gmail.com>
2020-06-01 12:53:22 -0700

    autoload services and lib in engine file

--- lib/spree_vendor_shopify_sync/engine.rb
+++ lib/spree_vendor_shopify_sync/engine.rb
@@ -9,6 +9,8 @@ module SpreeVendorShopifySync
       g.test_framework :rspec
     end
 
+    config.autoload_paths += %W[#{config.root}/lib #{config.root}/app/services]
+    
     def self.activate
       Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
