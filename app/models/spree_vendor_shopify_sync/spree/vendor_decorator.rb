f0019a3f4ed051ff84261d9342217d2f5845b307
Ashley <ashleyvdp@gmail.com>
2020-06-01 06:17:52 -0700

    add nonce back in and encrypted service

--- app/models/spree_vendor_shopify_sync/spree/vendor_decorator.rb
+++ app/models/spree_vendor_shopify_sync/spree/vendor_decorator.rb
@@ -1,8 +1,15 @@
 Spree::Vendor.class_eval do
+  include Encryptable
+  attr_encrypted :shopify_token
+
   after_save :log_sync
 
   has_many :sync_logs, as: :syncable
   has_many :product_sync_logs, through: :products, source: :sync_logs
+
+  def nonce
+    "#{name}-#{id}-#{created_at}"
+  end
 
   def log_sync
     if saved_change_to_shopify_token? || saved_change_to_shopify_domain?
--- db/migrate/20200501033435_add_encrypted_shopify_api_token_to_vendors.rb
+++ db/migrate/20200501033435_add_encrypted_shopify_api_token_to_vendors.rb
@@ -1,6 +1,6 @@
 class AddEncryptedShopifyApiTokenToVendors < ActiveRecord::Migration[6.0]
   def change
-  	add_column :spree_vendors, :shopify_token, :string
+  	add_column :spree_vendors, :encrypted_shopify_token, :string
   	add_column :spree_vendors, :shopify_domain, :string
   end
 end
